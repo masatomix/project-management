8cf0ad0c94f4cc8f631e787e49dd7f7d4b5532c8
masatomix <masatomix@83e30dcd-b832-0410-ae26-f3349284606d>
2008-05-21 07:19:55 +0000

    標準入力にタブがあっても無視しないよう修正。

--- trunk/MailFilter/main.sh
+++ trunk/MailFilter/main.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export JAVA_HOME=/opt/jdk1.5.0_12
+
 # MailFilterを置いたディレクトリに置き換える
 export LIBDIR=/home/hogehoge/MailFilter/lib
 
@@ -17,20 +18,21 @@ export PATH=${JAVA_HOME}/bin:${PATH}
 # クラスパスの設定 以上
 
 
-
-# こうやって標準出力からデータ取得すると、タブが置き換わってしまうため、とりあえずコメントアウト。
-#-----------------------
 # 標準入力から値を取得
-#tmp="";
+(
+tmp="";
 
-#while read line
-#do
-# tmp="${tmp}${line}\n";
-#done
+# 区切り文字の変更 tabが無視されるのを回避している
+IFS=$'\n';
+while read line
+do
+ tmp="${tmp}${line}\n";
+done
 
+echo -en "$tmp" | java  nu.mine.kino.mail.impl.Main
+)
 
-#echo -e "$tmp" |java  nu.mine.kino.mail.impl.Main 
 #-----------------------
-java  nu.mine.kino.mail.impl.Main
+#java  nu.mine.kino.mail.impl.Main
 
 #exit 0
