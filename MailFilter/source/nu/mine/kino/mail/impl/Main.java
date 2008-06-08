614afa9e4e3cb743a9785912fba83d3244bece23
masatomix <masatomix@users.noreply.github.com>
2008-06-08 16:16:04 +0000

    デバッグログを整理。

--- MailFilter/source/nu/mine/kino/mail/impl/Main.java
+++ MailFilter/source/nu/mine/kino/mail/impl/Main.java
@@ -1,9 +1,11 @@
 /*******************************************************************************
- * Copyright (c) 2008 Masatomi KINO. All rights reserved. 
- * $Id$
+ * Copyright (c) 2008 Masatomi KINO. All rights reserved. $Id: Main.java 158
+ * 2008-06-08 14:59:24Z masatomix $
  ******************************************************************************/
 // 作成日: 2008/05/18
 package nu.mine.kino.mail.impl;
+
+import org.apache.log4j.Logger;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
@@ -11,19 +13,27 @@ import java.io.IOException;
 import nu.mine.kino.mail.FilterException;
 import nu.mine.kino.mail.FilterManager;
 
-
 public class Main {
+    /**
+     * Logger for this class
+     */
+    private static final Logger logger = Logger.getLogger(Main.class);
+
     /**
      * @param args
      * @throws IOException
      * @throws FilterException
      */
     public static void main(String[] args) throws FilterException {
+        logger.debug("main(String[]) - start");
+
         String mailData = createStr();
         FilterManager filterManager = new FilterManager();
         // この箇所で、フィルタを追加することも可能。
         filterManager.doFilters(mailData);
         System.out.println(mailData);
+
+        logger.debug("main(String[]) - end");
     }
 
     private static String createStr() {
