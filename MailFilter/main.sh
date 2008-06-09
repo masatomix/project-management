683e0784cd181bb05a3ad7ae3d94e97a0b7442f2
masatomix <masatomix@users.noreply.github.com>
2008-06-09 14:22:40 +0000

    多少整形

--- MailFilter/main.sh
+++ MailFilter/main.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+# Javaのインストールディレクトリを指定
 export JAVA_HOME=/opt/jdk1.5.0_12
 export HOME_DIR=`echo $HOME`
 
-# MailFilterを置いたディレクトリに置き換える
+# MailFilterを置いたディレクトリに置き換えてください
 export LIBDIR=${HOME_DIR}/MailFilter/lib
 
+# ${LIBDIR}ディレクトリ自体にも、パスを通す
+export CLASSPATH=${LIBDIR}
 # クラスパスの設定 ${LIBDIR} 内のjarすべて通す
 jarList=`ls ${LIBDIR} | grep .jar`
-export CLASSPATH=${LIBDIR}
 for jarFile in ${jarList}
 do
   # echo  ${LIBDIR}/${jarFile}
@@ -30,15 +32,15 @@ do
  tmp="${tmp}${line}\n";
 done
 
-if 
+# nu.mine.kino.mail.impl.Main クラスが正常終了したら
+# post2blog.pl を呼び出す
+# いきなり実行しないよう、stubが設定されているので書き換えて下さい
+if
   echo -en "$tmp" | java  nu.mine.kino.mail.impl.Main
 then
 #  echo -en "$tmp" | ${HOME_DIR}/post2blog.pl
   echo -en "$tmp" | ${HOME_DIR}/MailFilter/stub.sh
 fi
 )
-
-#-----------------------
-#java  nu.mine.kino.mail.impl.Main
 
 exit 0
