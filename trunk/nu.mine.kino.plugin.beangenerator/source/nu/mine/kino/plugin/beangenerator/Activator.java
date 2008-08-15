72a95345d9257dde4ec6045c8e51661ef75fd4bb
masatomix <masatomix@83e30dcd-b832-0410-ae26-f3349284606d>
2008-08-15 08:49:14 +0000

    IFileからExcel情報を取得し、クラス情報(のリスト)を作成するメソッドを追加。

--- trunk/nu.mine.kino.plugin.beangenerator/source/nu/mine/kino/plugin/beangenerator/Activator.java
+++ trunk/nu.mine.kino.plugin.beangenerator/source/nu/mine/kino/plugin/beangenerator/Activator.java
@@ -1,5 +1,20 @@
 package nu.mine.kino.plugin.beangenerator;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.List;
+
+import net.java.amateras.xlsbeans.XLSBeans;
+import net.java.amateras.xlsbeans.XLSBeansException;
+import nu.mine.kino.exception.ApplicationException;
+import nu.mine.kino.utils.beangenerator.sheetdata.ClassInformation;
+import nu.mine.kino.utils.beangenerator.sheetdata.ClassInformationSheet;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -8,43 +23,63 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator extends AbstractUIPlugin {
 
-	// The plug-in ID
-	public static final String PLUGIN_ID = "nu.mine.kino.plugin.beangenerator";
+    // The plug-in ID
+    public static final String PLUGIN_ID = "nu.mine.kino.plugin.beangenerator";
 
-	// The shared instance
-	private static Activator plugin;
-	
-	/**
-	 * The constructor
-	 */
-	public Activator() {
-	}
+    // The shared instance
+    private static Activator plugin;
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;
-	}
+    /**
+     * The constructor
+     */
+    public Activator() {
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+     */
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        plugin = this;
+    }
 
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() {
-		return plugin;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+     */
+    public void stop(BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
+
+    /**
+     * Returns the shared instance
+     * 
+     * @return the shared instance
+     */
+    public static Activator getDefault() {
+        return plugin;
+    }
+
+    public List<ClassInformation> getClassInformations(IFile file)
+            throws CoreException {
+        File instance = file.getLocation().toFile();
+
+        try {
+            InputStream in = new FileInputStream(instance);
+            // ここでアノテーションからJavaBeansのマッピングがされ、インスタンスまで生成される
+            ClassInformationSheet sheet = new XLSBeans().load(in,
+                    ClassInformationSheet.class);
+            return sheet.getClassInformation();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (XLSBeansException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 }
