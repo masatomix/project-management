a09235fb94a313012a5726e19d6686c8f16db593
masatomix <masatomix@83e30dcd-b832-0410-ae26-f3349284606d>
2008-08-20 12:28:24 +0000

    クラス情報クラスのインタフェースを作成。

---
+++ trunk/nu.mine.kino.plugin.beangenerator/source/nu/mine/kino/plugin/beangenerator/sheetdata/IClassInformation.java
@@ -0,0 +1,42 @@
+/******************************************************************************
+ * Copyright (c) 2008 Masatomi KINO and others. 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ *      Masatomi KINO - initial API and implementation
+ * $Id$
+ ******************************************************************************/
+//çÏê¨ì˙: 2008/08/20
+package nu.mine.kino.plugin.beangenerator.sheetdata;
+
+import java.util.List;
+
+/**
+ * @author Masatomi KINO
+ * @version $Revision$
+ */
+public interface IClassInformation {
+
+    public void setFieldInformations(List<IFieldInformation> fieldInformations);
+
+    public void setClassNameJ(String classNameJ);
+
+    public void setDescription(String description);
+
+    public void setPackageName(String packageName);
+
+    public void setClassName(String className);
+
+    public List<IFieldInformation> getFieldInformations();
+
+    public String getClassNameJ();
+
+    public String getDescription();
+
+    public String getPackageName();
+
+    public String getClassName();
+
+}
