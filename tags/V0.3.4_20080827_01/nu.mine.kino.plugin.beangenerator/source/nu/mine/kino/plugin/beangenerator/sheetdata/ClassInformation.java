839e679f5272b74ca63c889c1b3d6e0f9c1d1bc7
masatomix <masatomix@83e30dcd-b832-0410-ae26-f3349284606d>
2008-08-27 02:14:13 +0000

    JavaBeans Creator Plugin のVer 0.3.4をリリースしたのでタグ付け。独自のアノテーションを付加できるよう対応。

---
+++ tags/V0.3.4_20080827_01/nu.mine.kino.plugin.beangenerator/source/nu/mine/kino/plugin/beangenerator/sheetdata/ClassInformation.java
@@ -0,0 +1,101 @@
+/******************************************************************************
+ * Copyright (c) 2008 Masatomi KINO and others. 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ *      Masatomi KINO - initial API and implementation
+ * $Id$
+ ******************************************************************************/
+// 作成日: 2008/08/15
+package nu.mine.kino.plugin.beangenerator.sheetdata;
+
+import java.util.List;
+
+import net.java.amateras.xlsbeans.annotation.HorizontalRecords;
+import net.java.amateras.xlsbeans.annotation.LabelledCell;
+import net.java.amateras.xlsbeans.annotation.LabelledCellType;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+/**
+ * @author Masatomi KINO
+ * @version $Revision$
+ */
+public class ClassInformation implements IClassInformation {
+
+    private List<IFieldInformation> fieldInformations;
+
+    private String classNameJ;
+
+    private String description;
+
+    private String packageName;
+
+    private String className;
+
+    private String toString;
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append("クラス名日本語", classNameJ).append( //$NON-NLS-1$
+                "説明", description).append("パッケージ名", packageName).append("クラス名", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+                className).append(fieldInformations.toArray()).toString();
+    }
+
+    @HorizontalRecords(tableLabel = "クラス情報", recordClass = FieldInformation.class)//$NON-NLS-1$
+    public void setFieldInformations(List<IFieldInformation> fieldInformations) {
+        this.fieldInformations = fieldInformations;
+    }
+
+    @LabelledCell(label = "クラス名日本語", type = LabelledCellType.Right)//$NON-NLS-1$
+    public void setClassNameJ(String classNameJ) {
+        this.classNameJ = classNameJ;
+    }
+
+    @LabelledCell(label = "説明", type = LabelledCellType.Right)//$NON-NLS-1$
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @LabelledCell(label = "パッケージ名", type = LabelledCellType.Right)//$NON-NLS-1$
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
+    }
+
+    @LabelledCell(label = "クラス名", type = LabelledCellType.Right)//$NON-NLS-1$
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    @LabelledCell(label = "toString", type = LabelledCellType.Right)//$NON-NLS-1$
+    public void setToString(String toString) {
+        this.toString = toString;
+    }
+
+    public List<IFieldInformation> getFieldInformations() {
+        return fieldInformations;
+    }
+
+    public String getClassNameJ() {
+        return classNameJ;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public String getToString() {
+        return toString;
+    }
+
+}
