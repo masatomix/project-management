a98f7bd4ca06c55bcf88bbe6394c2ce89d728940
masatomix <masatomix@users.noreply.github.com>
2008-08-29 12:15:03 +0000

    内部のJavaBeansを、実際にこのプラグインで作成したものに、差し替えた

--- nu.mine.kino.plugin.beangenerator/source/nu/mine/kino/plugin/beangenerator/sheetdata/FieldInformation.java
+++ nu.mine.kino.plugin.beangenerator/source/nu/mine/kino/plugin/beangenerator/sheetdata/FieldInformation.java
@@ -8,7 +8,7 @@
  *      Masatomi KINO - initial API and implementation
  * $Id$
  ******************************************************************************/
-// 作成日: 2008/08/15
+
 package nu.mine.kino.plugin.beangenerator.sheetdata;
 
 import net.java.amateras.xlsbeans.annotation.Column;
@@ -16,60 +16,117 @@ import net.java.amateras.xlsbeans.annotation.Column;
 import org.apache.commons.lang.builder.ToStringBuilder;
 
 /**
- * @author Masatomi KINO
+ * フィールド情報を格納するクラスです
+ * 
+ * @author Masatomi KINO and JavaBeans Creator Plug-in
  * @version $Revision$
  */
 public class FieldInformation implements IFieldInformation {
 
+    /**
+     * フィールド情報のリストです
+     */
     private String fieldNameJ;
 
+    /**
+     * 日本語のクラス名を格納するフィールドです。
+     */
     private String fieldName;
 
+    /**
+     * 説明のフィールドです
+     */
     private String fieldType;
 
+    /**
+     * 説明のフィールドです
+     */
     private String description;
 
-    @Column(columnName = "フィールド名日本語")//$NON-NLS-1$
+    /**
+     * フィールド名日本語をセットする。
+     * 
+     * @param fieldNameJ
+     *            フィールド名日本語
+     */
+    @Column(columnName = "フィールド名日本語")
     public void setFieldNameJ(String fieldNameJ) {
         this.fieldNameJ = fieldNameJ;
     }
 
-    @Column(columnName = "フィールド名")//$NON-NLS-1$
+    /**
+     * フィールド名をセットする。
+     * 
+     * @param fieldName
+     *            フィールド名
+     */
+    @Column(columnName = "フィールド名")
     public void setFieldName(String fieldName) {
         this.fieldName = fieldName;
     }
 
-    @Column(columnName = "型")//$NON-NLS-1$
+    /**
+     * 型をセットする。
+     * 
+     * @param fieldType
+     *            型
+     */
+    @Column(columnName = "型")
     public void setFieldType(String fieldType) {
         this.fieldType = fieldType;
     }
 
-    @Column(columnName = "説明")//$NON-NLS-1$
+    /**
+     * 説明をセットする。
+     * 
+     * @param description
+     *            説明
+     */
+    @Column(columnName = "説明")
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * フィールド名日本語を取得する。
+     * 
+     * @return フィールド名日本語
+     */
     public String getFieldNameJ() {
         return fieldNameJ;
     }
 
+    /**
+     * フィールド名を取得する。
+     * 
+     * @return フィールド名
+     */
     public String getFieldName() {
         return fieldName;
     }
 
+    /**
+     * 型を取得する。
+     * 
+     * @return 型
+     */
     public String getFieldType() {
         return fieldType;
     }
 
+    /**
+     * 説明を取得する。
+     * 
+     * @return 説明
+     */
     public String getDescription() {
         return description;
     }
 
     @Override
     public String toString() {
-        return new ToStringBuilder(this).append("フィールド名日本語", fieldNameJ) //$NON-NLS-1$
-                .append("説明", description).append("フィールド名", fieldName).append( //$NON-NLS-1$ //$NON-NLS-2$
-                        "型", fieldType).toString(); //$NON-NLS-1$
+        return new ToStringBuilder(this).append("フィールド名日本語", fieldNameJ)
+                .append("フィールド名", fieldName).append("型", fieldType).append(
+                        "説明", description).toString();
     }
-
 }
