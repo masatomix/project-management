7ca350dde17d66bb56efe67e33a25a9f89da31d8
masatomix <masatomix@users.noreply.github.com>
2009-05-01 12:32:24 +0000

    コメント修正。

--- nu.mine.kino.plugin.plugindocumentcreator/source/nu/mine/kino/plugin/plugindocumentcreator/DefaultExtensionPointFormatter.java
+++ nu.mine.kino.plugin.plugindocumentcreator/source/nu/mine/kino/plugin/plugindocumentcreator/DefaultExtensionPointFormatter.java
@@ -1,8 +1,13 @@
-/*******************************************************************************
- * Copyright (c) 2006 Masatomi KINO.
- * All rights reserved. 
+/******************************************************************************
+ * Copyright (c) 2009 Masatomi KINO and others. 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ *      Masatomi KINO - initial API and implementation
  * $Id$
- *******************************************************************************/
+ ******************************************************************************/
 //çÏê¨ì˙: 2006/10/22
 package nu.mine.kino.plugin.plugindocumentcreator;
 
