e5925440c9274044b08ac6833c8d8ff4e5a01bc7
masatomix <masatomix@users.noreply.github.com>
2015-02-02 16:30:51 +0000

    POIによる値の上書きを実施。

---
+++ EVMTools/src/main/java/nu/mine/kino/entity/ExcelPOIScheduleBean.java
@@ -0,0 +1,322 @@
+/******************************************************************************
+ * Copyright (c) 2008-2014 Masatomi KINO and others. 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ *      Masatomi KINO - initial API and implementation
+ ******************************************************************************/
+
+package nu.mine.kino.entity;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+/**
+ * 
+ * @author Masatomi KINO and JavaBeans Creator Plug-in
+ */
+public class ExcelPOIScheduleBean {
+
+    /**
+     * ﾀｽｸID
+     */
+    private String taskId;
+
+    /**
+     * 予定工数
+     */
+    private Double numberOfManDays;
+
+    /**
+     * 予定開始日
+     */
+    private java.util.Date scheduledStartDate;
+
+    /**
+     * 予定終了日
+     */
+    private java.util.Date scheduledEndDate;
+
+    /**
+     * 実績開始日
+     */
+    private java.util.Date startDate;
+
+    /**
+     * 実績終了日
+     */
+    private java.util.Date endDate;
+
+    /**
+     * 進捗率
+     */
+    private Double progressRate;
+
+    /**
+     * 稼動予定日数
+     */
+    private Integer numberOfDays;
+
+    /**
+     * Planned Value
+     */
+    private Double plannedValue;
+
+    /**
+     * Earned Value
+     */
+    private Double earnedValue;
+
+    /**
+     * Actual Cost
+     */
+    private Double actualCost;
+
+    /**
+     * 基準日
+     */
+    private java.util.Date baseDate;
+
+    /**
+     * ﾀｽｸIDをセットする。
+     * 
+     * @param taskId
+     *            ﾀｽｸID
+     */
+    public void setTaskId(String taskId) {
+        this.taskId = taskId;
+    }
+
+    /**
+     * 予定工数をセットする。
+     * 
+     * @param numberOfManDays
+     *            予定工数
+     */
+    public void setNumberOfManDays(Double numberOfManDays) {
+        this.numberOfManDays = numberOfManDays;
+    }
+
+    /**
+     * 予定開始日をセットする。
+     * 
+     * @param scheduledStartDate
+     *            予定開始日
+     */
+    public void setScheduledStartDate(java.util.Date scheduledStartDate) {
+        this.scheduledStartDate = scheduledStartDate;
+    }
+
+    /**
+     * 予定終了日をセットする。
+     * 
+     * @param scheduledEndDate
+     *            予定終了日
+     */
+    public void setScheduledEndDate(java.util.Date scheduledEndDate) {
+        this.scheduledEndDate = scheduledEndDate;
+    }
+
+    /**
+     * 実績開始日をセットする。
+     * 
+     * @param startDate
+     *            実績開始日
+     */
+    public void setStartDate(java.util.Date startDate) {
+        this.startDate = startDate;
+    }
+
+    /**
+     * 実績終了日をセットする。
+     * 
+     * @param endDate
+     *            実績終了日
+     */
+    public void setEndDate(java.util.Date endDate) {
+        this.endDate = endDate;
+    }
+
+    /**
+     * 進捗率をセットする。
+     * 
+     * @param progressRate
+     *            進捗率
+     */
+    public void setProgressRate(Double progressRate) {
+        this.progressRate = progressRate;
+    }
+
+    /**
+     * 稼動予定日数をセットする。
+     * 
+     * @param numberOfDays
+     *            稼動予定日数
+     */
+    public void setNumberOfDays(Integer numberOfDays) {
+        this.numberOfDays = numberOfDays;
+    }
+
+    /**
+     * Planned Valueをセットする。
+     * 
+     * @param plannedValue
+     *            Planned Value
+     */
+    public void setPlannedValue(Double plannedValue) {
+        this.plannedValue = plannedValue;
+    }
+
+    /**
+     * Earned Valueをセットする。
+     * 
+     * @param earnedValue
+     *            Earned Value
+     */
+    public void setEarnedValue(Double earnedValue) {
+        this.earnedValue = earnedValue;
+    }
+
+    /**
+     * Actual Costをセットする。
+     * 
+     * @param actualCost
+     *            Actual Cost
+     */
+    public void setActualCost(Double actualCost) {
+        this.actualCost = actualCost;
+    }
+
+    /**
+     * 基準日をセットする。
+     * 
+     * @param baseDate
+     *            基準日
+     */
+    public void setBaseDate(java.util.Date baseDate) {
+        this.baseDate = baseDate;
+    }
+
+    /**
+     * ﾀｽｸIDを取得する。
+     * 
+     * @return ﾀｽｸID
+     */
+    public String getTaskId() {
+        return taskId;
+    }
+
+    /**
+     * 予定工数を取得する。
+     * 
+     * @return 予定工数
+     */
+    public Double getNumberOfManDays() {
+        return numberOfManDays;
+    }
+
+    /**
+     * 予定開始日を取得する。
+     * 
+     * @return 予定開始日
+     */
+    public java.util.Date getScheduledStartDate() {
+        return scheduledStartDate;
+    }
+
+    /**
+     * 予定終了日を取得する。
+     * 
+     * @return 予定終了日
+     */
+    public java.util.Date getScheduledEndDate() {
+        return scheduledEndDate;
+    }
+
+    /**
+     * 実績開始日を取得する。
+     * 
+     * @return 実績開始日
+     */
+    public java.util.Date getStartDate() {
+        return startDate;
+    }
+
+    /**
+     * 実績終了日を取得する。
+     * 
+     * @return 実績終了日
+     */
+    public java.util.Date getEndDate() {
+        return endDate;
+    }
+
+    /**
+     * 進捗率を取得する。
+     * 
+     * @return 進捗率
+     */
+    public Double getProgressRate() {
+        return progressRate;
+    }
+
+    /**
+     * 稼動予定日数を取得する。
+     * 
+     * @return 稼動予定日数
+     */
+    public Integer getNumberOfDays() {
+        return numberOfDays;
+    }
+
+    /**
+     * Planned Valueを取得する。
+     * 
+     * @return Planned Value
+     */
+    public Double getPlannedValue() {
+        return plannedValue;
+    }
+
+    /**
+     * Earned Valueを取得する。
+     * 
+     * @return Earned Value
+     */
+    public Double getEarnedValue() {
+        return earnedValue;
+    }
+
+    /**
+     * Actual Costを取得する。
+     * 
+     * @return Actual Cost
+     */
+    public Double getActualCost() {
+        return actualCost;
+    }
+
+    /**
+     * 基準日を取得する。
+     * 
+     * @return 基準日
+     */
+    public java.util.Date getBaseDate() {
+        return baseDate;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append("ﾀｽｸID", taskId)
+                .append("予定工数", numberOfManDays)
+                .append("予定開始日", scheduledStartDate)
+                .append("予定終了日", scheduledEndDate).append("実績開始日", startDate)
+                .append("実績終了日", endDate).append("進捗率", progressRate)
+                .append("稼動予定日数", numberOfDays)
+                .append("Planned Value", plannedValue)
+                .append("Earned Value", earnedValue)
+                .append("Actual Cost", actualCost).append("基準日", baseDate)
+                .toString();
+    }
+}
---
+++ EVMTools/src/main/java/nu/mine/kino/entity/Row2ExcelPOIScheduleBean.java
@@ -0,0 +1,112 @@
+/******************************************************************************
+ * Copyright (c) 2008-2014 Masatomi KINO and others. 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ *      Masatomi KINO - initial API and implementation
+ ******************************************************************************/
+
+package nu.mine.kino.entity;
+
+import org.apache.poi.ss.usermodel.Row;
+
+public class Row2ExcelPOIScheduleBean {
+
+    /**
+     * 引数のオブジェクトのプロパティからデータをコピーして戻り値のオブジェクトを生成して返すメソッド。
+     * 
+     * @param source
+     * @return
+     */
+    public static ExcelPOIScheduleBean convert(Row source) {
+        ExcelPOIScheduleBean dest = new ExcelPOIScheduleBean();
+
+        // 必要に応じて特殊な載せ替え処理 開始
+        ((ExcelPOIScheduleBean) dest)
+                .setTaskId(nu.mine.kino.projects.utils.PoiUtils
+                        .getTaskId(source.getCell(1)));
+        ((ExcelPOIScheduleBean) dest)
+                .setNumberOfManDays((Double) nu.mine.kino.projects.utils.PoiUtils
+                        .getCellValue(source.getCell(15), Double.class));
+        ((ExcelPOIScheduleBean) dest)
+                .setProgressRate((Double) nu.mine.kino.projects.utils.PoiUtils
+                        .getCellValue(source.getCell(20), Double.class));
+        ((ExcelPOIScheduleBean) dest)
+                .setNumberOfDays((Integer) nu.mine.kino.projects.utils.PoiUtils
+                        .getCellValue(source.getCell(21), Integer.class));
+        ((ExcelPOIScheduleBean) dest)
+                .setPlannedValue((Double) nu.mine.kino.projects.utils.PoiUtils
+                        .getCellValue(source.getCell(22), Double.class));
+        ((ExcelPOIScheduleBean) dest)
+                .setEarnedValue((Double) nu.mine.kino.projects.utils.PoiUtils
+                        .getCellValue(source.getCell(23), Double.class));
+        ((ExcelPOIScheduleBean) dest)
+                .setActualCost((Double) nu.mine.kino.projects.utils.PoiUtils
+                        .getCellValue(source.getCell(24), Double.class));
+        ((ExcelPOIScheduleBean) dest)
+                .setScheduledStartDate(nu.mine.kino.projects.utils.PoiUtils
+                        .getDate(source.getCell(16)));
+        ((ExcelPOIScheduleBean) dest)
+                .setScheduledEndDate(nu.mine.kino.projects.utils.PoiUtils
+                        .getDate(source.getCell(17)));
+        ((ExcelPOIScheduleBean) dest)
+                .setStartDate(nu.mine.kino.projects.utils.PoiUtils
+                        .getDate(source.getCell(18)));
+        ((ExcelPOIScheduleBean) dest)
+                .setEndDate(nu.mine.kino.projects.utils.PoiUtils.getDate(source
+                        .getCell(19)));
+
+        // 特殊な載せ替え処理 終了
+
+        return dest;
+    }
+
+    /**
+     * 第一引数から第二引数へプロパティをコピーするメソッド。
+     * 
+     * @param source
+     * @param dest
+     */
+    public static void convert(Row source, ExcelPOIScheduleBean dest) {
+        // 必要に応じて特殊な載せ替え処理 開始
+        ((ExcelPOIScheduleBean) dest)
+                .setTaskId(nu.mine.kino.projects.utils.PoiUtils
+                        .getTaskId(source.getCell(1)));
+        ((ExcelPOIScheduleBean) dest)
+                .setNumberOfManDays((Double) nu.mine.kino.projects.utils.PoiUtils
+                        .getCellValue(source.getCell(15), Double.class));
+        ((ExcelPOIScheduleBean) dest)
+                .setProgressRate((Double) nu.mine.kino.projects.utils.PoiUtils
+                        .getCellValue(source.getCell(20), Double.class));
+        ((ExcelPOIScheduleBean) dest)
+                .setNumberOfDays((Integer) nu.mine.kino.projects.utils.PoiUtils
+                        .getCellValue(source.getCell(21), Integer.class));
+        ((ExcelPOIScheduleBean) dest)
+                .setPlannedValue((Double) nu.mine.kino.projects.utils.PoiUtils
+                        .getCellValue(source.getCell(22), Double.class));
+        ((ExcelPOIScheduleBean) dest)
+                .setEarnedValue((Double) nu.mine.kino.projects.utils.PoiUtils
+                        .getCellValue(source.getCell(23), Double.class));
+        ((ExcelPOIScheduleBean) dest)
+                .setActualCost((Double) nu.mine.kino.projects.utils.PoiUtils
+                        .getCellValue(source.getCell(24), Double.class));
+        ((ExcelPOIScheduleBean) dest)
+                .setScheduledStartDate(nu.mine.kino.projects.utils.PoiUtils
+                        .getDate(source.getCell(16)));
+        ((ExcelPOIScheduleBean) dest)
+                .setScheduledEndDate(nu.mine.kino.projects.utils.PoiUtils
+                        .getDate(source.getCell(17)));
+        ((ExcelPOIScheduleBean) dest)
+                .setStartDate(nu.mine.kino.projects.utils.PoiUtils
+                        .getDate(source.getCell(18)));
+        ((ExcelPOIScheduleBean) dest)
+                .setEndDate(nu.mine.kino.projects.utils.PoiUtils.getDate(source
+                        .getCell(19)));
+
+        // 特殊な載せ替え処理 終了
+
+    }
+
+}
