a650bd3edb26a8a4aac214aef6ee08d7cde50e97
masatomix <masatomix@83e30dcd-b832-0410-ae26-f3349284606d>
2015-02-09 10:09:29 +0000

    GUIの改善。リンクの改善。リファクタリング

--- trunk/project-management/src/main/java/nu/mine/kino/jenkins/plugins/projectmanagement/ProjectSummaryProjectAction.java
+++ trunk/project-management/src/main/java/nu/mine/kino/jenkins/plugins/projectmanagement/ProjectSummaryProjectAction.java
@@ -4,11 +4,17 @@ import hudson.model.Action;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import nu.mine.kino.entity.PVACEVViewBean;
 import nu.mine.kino.jenkins.plugins.projectmanagement.utils.PMUtils;
+import nu.mine.kino.projects.utils.ReadUtils;
 
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
@@ -42,6 +48,26 @@ public class ProjectSummaryProjectAction implements Action {
     // public String getData() {
     // return "Sample Project Action!!";
     // }
+
+    public ProjectSummaryAction[] getSeriesActions() throws IOException {
+        List<ProjectSummaryAction> actions = new ArrayList<ProjectSummaryAction>();
+        String file = PMConstants.SERIES_DAT_FILENAME;
+        AbstractBuild<?, ?> build = PMUtils.findBuild(project, file);
+        if (build == null) {
+            return new ProjectSummaryAction[0];
+        }
+        String data = ReadUtils.readFile(new File(build.getRootDir(), file));
+        BufferedReader reader = new BufferedReader(new StringReader(data));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] split = line.split("\t");
+            String buildNumber = split[1];
+            AbstractBuild<?, ?> record = project.getBuildByNumber(Integer
+                    .parseInt(buildNumber));
+            actions.add(record.getAction(ProjectSummaryAction.class));
+        }
+        return actions.toArray(new ProjectSummaryAction[actions.size()]);
+    }
 
     public PVACEVViewBean getCurrentPVACEV() {
         final AbstractBuild<?, ?> tb = project.getLastSuccessfulBuild();
@@ -77,14 +103,21 @@ public class ProjectSummaryProjectAction implements Action {
         return null;
     }
 
+    /**
+     * 左メニューのリンクを無理矢理下記の画面へ遷移させている。本来はindex.jellyに行く。
+     * 
+     * @param request
+     * @param response
+     * @throws IOException
+     */
     public void doIndex(final StaplerRequest request,
             final StaplerResponse response) throws IOException {
         AbstractBuild<?, ?> build = project.getLastSuccessfulBuild();
 
         String path = String.format("/job/%s/%d/%s", build.getProject()
                 .getName(), build.getNumber(), "project-summary");
-        System.out.println(path);
-        System.out.println(request.getContextPath());
+        // System.out.println(path);
+        // System.out.println(request.getContextPath());
         response.sendRedirect2(request.getContextPath() + path);
     }
 }
