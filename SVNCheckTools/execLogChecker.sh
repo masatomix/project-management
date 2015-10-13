13a0d8c2b69e3212c5ae9166f52aa4d1e758c153
masatomix <masatomix@users.noreply.github.com>
2015-10-13 00:51:08 +0000

    微調整したので再タグ付け

--- SVNCheckTools/execLogChecker.sh
+++
@@ -1,1 +0,0 @@
-java -jar ./target/SVNCheckTools-0.0.1-jar-with-dependencies.jar -U https://www.masatom.in/svnsamples/repo/trunk/ -u user -p pass -f 20141128 -t 20141130 revision date author type path 
